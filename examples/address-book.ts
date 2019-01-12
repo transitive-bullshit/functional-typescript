a9f855126bd85e7f2a7b5fb3aa5b32199f6f3957
Travis Fischer <fisch0920@gmail.com>
2019-01-12 02:54:49 -0500

    init

---
+++ examples/address-book.ts
@@ -0,0 +1,37 @@
+/**
+ * #TopLevel
+ */
+export default function Nala (address: AddressBook): string {
+  return 'TODO'
+}
+
+class AddressBook {
+  /**
+   * A dictionary of Contacts, indexed by unique ID
+   */
+  contacts: { [id: string]: Contact };
+}
+
+class Contact {
+  firstName: string;
+  lastName?: string;
+
+  birthday?: Date;
+  title?: "Mr." | "Mrs." | "Ms." | "Prof.";
+
+  emails: string[];
+  phones: PhoneNumber[];
+
+  /** @TJS-type integer */
+  highScore: number;
+}
+
+/**
+ * A Contact's phone number.
+ */
+class PhoneNumber {
+  number: string;
+
+  /** An optional label (e.g. "mobile") */
+  label?: string;
+}
---
+++ examples/primitive-types.ts
@@ -0,0 +1,10 @@
+enum Color { Red, Green, Blue }
+
+interface Nala {
+  numbers: number[]
+  color: Color
+}
+
+export default function Foo (foo: string, bar: number, nala: Nala): string {
+  return 'TODO'
+}
